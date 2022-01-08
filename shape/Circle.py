58f0fed0219e7a905fb5a59a8a4b0448aa0e5dfe
Michael Tran <solidwater969@gmail.com>
2022-01-08 16:55:40 -0500

    refactor: change spacing

--- shape/Circle.py
+++ shape/Circle.py
@@ -1,24 +1,25 @@
 import pygame
 from shape.Shape import Shape
 
+
 class Circle(Shape):
-  def __init__(self, color, position, radius):
-    super().__init__(color, position)
-    self.__radius = radius
+    def __init__(self, color, position, radius):
+        super().__init__(color, position)
+        self.__radius = radius
 
-  def getRadius(self):
-    return self.__radius
+    def getRadius(self):
+        return self.__radius
 
-  def __str__(self):
-    return 'Circle(%r, %r, %r)' % (self.getColor(), self.getPosition(), self.__radius)
+    def __str__(self):
+        return 'Circle(%r, %r, %r)' % (self.getColor(), self.getPosition(), self.__radius)
 
-  def draw(self, surface):
-    pygame.draw.circle(surface, self.getColor(), self.getPosition(), self.__radius)
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.getColor(), self.getPosition(), self.__radius)
 
-  def getHitbox(self):
-    return pygame.Rect(
-      self.getPosition()[0] - self.__radius,
-      self.getPosition()[1] - self.__radius,
-      self.__radius * 2,
-      self.__radius * 2
-    )
+    def getHitbox(self):
+        return pygame.Rect(
+            self.getPosition()[0] - self.__radius,
+            self.getPosition()[1] - self.__radius,
+            self.__radius * 2,
+            self.__radius * 2
+        )
--- shape/Shape.py
+++ shape/Shape.py
@@ -1,37 +1,38 @@
 from abc import ABC, abstractmethod
 
+
 class Shape(ABC):
-  def __init__(self, color, position):
-    self.__color = color
-    self.__position = position
+    def __init__(self, color, position):
+        self.__color = color
+        self.__position = position
 
-  def getColor(self):
-    return self.__color
+    def getColor(self):
+        return self.__color
 
-  def getPosition(self):
-    return self.__position
+    def getPosition(self):
+        return self.__position
 
-  def setPosition(self, position):
-    self.__position = position
+    def setPosition(self, position):
+        self.__position = position
 
-  @abstractmethod
-  def draw(self, surface):
-    pass
+    @abstractmethod
+    def draw(self, surface):
+        pass
 
-  @abstractmethod
-  def getHitbox(self):
-    pass
+    @abstractmethod
+    def getHitbox(self):
+        pass
 
-  def isTouched(self, shape):
-    if (self is shape):
-      return False
+    def isTouched(self, shape):
+        if (self is shape):
+            return False
 
-    hitbox0 = self.getHitbox()
-    hitbox1 = shape.getHitbox()
+        hitbox0 = self.getHitbox()
+        hitbox1 = shape.getHitbox()
 
-    return (
-      (hitbox0.x < hitbox1.x + hitbox1.width) and
-      (hitbox0.y < hitbox1.y + hitbox1.height) and
-      (hitbox1.x < hitbox0.x + hitbox0.width) and
-      (hitbox1.y < hitbox0.y + hitbox0.height)
-    )
+        return (
+                (hitbox0.x < hitbox1.x + hitbox1.width) and
+                (hitbox0.y < hitbox1.y + hitbox1.height) and
+                (hitbox1.x < hitbox0.x + hitbox0.width) and
+                (hitbox1.y < hitbox0.y + hitbox0.height)
+        )
